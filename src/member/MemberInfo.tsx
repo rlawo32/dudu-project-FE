6745a88bb358640ffc8603167d8c868c1610d18c
rlawo <rlawo32@naver.com>
2024-01-31 21:32:04 +0900

    [DuDu-project][FE] <회원> 회원탈퇴_작업_20240131 (3)

--- src/member/MemberInfo.tsx
+++ src/member/MemberInfo.tsx
@@ -1,17 +1,56 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
 
 import FooterNavigation from "../navigation/FooterNavigation";
 import HeaderNavigation from "../navigation/HeaderNavigation";
 import TopButtonNavigation from "../navigation/TopButtonNavigation";
+import MemberInfoUpdate from "./memberInfoCompnent/MemberInfoUpdate";
+import MemberInfoPwUpdate from "./memberInfoCompnent/MemberInfoPwUpdate";
+import MemberInfoWithdraw from "./memberInfoCompnent/MemberInfoWithdraw";
 
 import * as Styled from "./MemberInfo.style";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faBasketShopping as basketIcon, faChalkboardUser as reviewIcon,
 faClipboard as historyIcon, faClockRotateLeft as logIcon} from "@fortawesome/free-solid-svg-icons";
-import {useNavigate} from "react-router-dom";
 
 const MemberInfo = () => {
     const navigate = useNavigate();
+
+    const [memberInfo, setMemberInfo] = useState<{
+        memberNo:number;
+        memberEmail:string;
+        memberId:string;
+        memberName:string;
+        memberGender:string;
+        memberBirth:string;
+        memberPhone:string;
+    }>({
+        memberNo: 0,
+        memberEmail: '',
+        memberId: '',
+        memberName: '',
+        memberGender: '',
+        memberBirth: '',
+        memberPhone: ''
+    });
+    const [isInfoUpdateModal, setIsInfoUpdateModal] = useState<boolean>(false);
+    const [isInfoPwUpdateModal, setIsInfoPwUpdateModal] = useState<boolean>(false);
+    const [isInfoWithdrawModal, setIsInfoWithdrawModal] = useState<boolean>(false);
+
+    useEffect(() => {
+        const memberInfoData = async ():Promise<void> => {
+            await axios({
+                method: "GET",
+                url: "/member/findMemberInfo"
+            }).then((res):void => {
+                setMemberInfo(res.data.data);
+            }).catch((err):void => {
+                console.log(err.message);
+            })
+        }
+        setTimeout(() => {memberInfoData().then();}, 200);
+    }, [])
 
     return (
         <Styled.MemberInfoView>
@@ -36,19 +75,21 @@ const MemberInfo = () => {
                             <div className="content-item mi-name">
                                 <span>이름</span>
                                 <div>
-                                    김성재
+                                    {memberInfo.memberName}
                                 </div>
                             </div>
                             <div className="content-item mi-birth">
                                 <span>생년월일</span>
                                 <div>
-                                    1997.07.16
+                                    {memberInfo.memberBirth}
                                 </div>
                             </div>
                             <div className="content-item mi-phone">
                                 <span>휴대전화</span>
                                 <div>
-                                    010-2208-4803
+                                    {memberInfo?.memberPhone.substring(0, 3) + "-" +
+                                        memberInfo?.memberPhone.substring(3, 7) + "-" +
+                                        memberInfo?.memberPhone.substring(7)}
                                 </div>
                             </div>
                         </div>
@@ -56,29 +97,39 @@ const MemberInfo = () => {
                             <div className="content-item mi-id">
                                 <span>아이디</span>
                                 <div>
-                                    rlawo32
+                                    {memberInfo.memberId}
                                 </div>
                             </div>
                             <div className="content-item mi-email">
                                 <span>이메일</span>
                                 <div>
-                                    rlawo32@naver.com
+                                    {memberInfo.memberEmail}
                                 </div>
                             </div>
                             <div className="content-item mi-gender">
                                 <span>성별</span>
                                 <div>
-                                    남
+                                    {memberInfo?.memberGender === 'M' ? "남" : "여"}
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+
+                <div className="mi-modal-section">
+                    {isInfoUpdateModal ? <MemberInfoUpdate setIsModal={setIsInfoUpdateModal} memberData={memberInfo}/> : <div/>}
+                    {isInfoPwUpdateModal ? <MemberInfoPwUpdate setIsModal={setIsInfoPwUpdateModal} memberId={memberInfo.memberId}/> : <div/>}
+                    {isInfoWithdrawModal ? <MemberInfoWithdraw setIsModal={setIsInfoWithdrawModal} memberId={memberInfo.memberId}/> : <div/>}
+                </div>
+
                 <div className="mi-info-update">
                     <div className="mi-button-section">
-                        <button style={{borderColor: "red"}}>회원 탈퇴</button>
-                        <button>비밀번호 변경</button>
-                        <button>회원정보 변경</button>
+                        <button onClick={() => {window.scrollTo({ top: 50, behavior: "smooth" });
+                            setIsInfoWithdrawModal(true);}} style={{borderColor: "red"}}>회원 탈퇴</button>
+                        <button onClick={() => {window.scrollTo({ top: 50, behavior: "smooth" });
+                            setIsInfoPwUpdateModal(true);}}>비밀번호 변경</button>
+                        <button onClick={() => {window.scrollTo({ top: 50, behavior: "smooth" });
+                            setIsInfoUpdateModal(true);}}>회원정보 변경</button>
                     </div>
                 </div>
 
