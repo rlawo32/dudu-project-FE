f59645b658c8144d4b1ad4b9ac2e2c98cddb3fbc
rlawo <rlawo32@naver.com>
2024-02-02 12:49:57 +0900

    [DuDu-project][FE] <기능> 비밀번호타입_작업_20240202 (5)

--- src/member/signUpComponent/EnterInfo.tsx
+++ src/member/signUpComponent/EnterInfo.tsx
@@ -1,11 +1,15 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import axios from "axios";
 
 import useJoinProgressStore from "../../stores/useJoinProgressStore";
-import * as Styled from "../SignUp.style";
 
+import * as Styled from "../SignUp.style";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEye as passwordSeeIcon} from "@fortawesome/free-solid-svg-icons";
 
 const EnterInfo = ():any => {
+    const passwordRef:any = useRef<any>();
+    const passwordChkRef:any = useRef<any>();
 
     // 회원가입 값
     const [memberId, setMemberId] = useState<string>("");
@@ -108,7 +112,7 @@ const EnterInfo = ():any => {
         setMemberPw(currentData);
 
         if(!regexChk.test(currentData)) {
-            setMemberPwMessage('비밀번호를 다시 확인해주세요.');
+            setMemberPwMessage('8~16자 영문 대 소문자, 숫자, 특수문자를 조합해주세요.');
             setIsMemberPwEffect(false);
         } else {
             setMemberPwMessage('');
@@ -181,6 +185,24 @@ const EnterInfo = ():any => {
         }
     }
 
+    const passwordSeeHandler = (type:string):void => {
+        if(type === 'chk') {
+            const typeCheck = passwordChkRef.current.type;
+            if(typeCheck === 'password') {
+                passwordChkRef.current.type = 'text';
+            } else {
+                passwordChkRef.current.type = 'password';
+            }
+        } else {
+            const typeCheck = passwordRef.current.type;
+            if(typeCheck === 'password') {
+                passwordRef.current.type = 'text';
+            } else {
+                passwordRef.current.type = 'password';
+            }
+        }
+    }
+
     const signUpHandler = ():void => {
 
         const signUpData:object = {
@@ -247,7 +269,7 @@ const EnterInfo = ():any => {
                     <span className="input-title">아이디</span>
                     <div className="input-text">
                         <Styled.EnterInfoInput type="text" onChange={(data) => memberIdRegex(data.target.value)} placeholder="아이디"
-                                               style={ isMemberIdEffect ? {} : {border: '2px solid red'} } onBlur={memberIdDuplicationHandler} />
+                                               style={ isMemberIdEffect ? {} : {border: '3px solid red'} } onBlur={memberIdDuplicationHandler} />
                         <div style={  isMemberIdEffect ? {display:'none'} : {display:'block', color:'red', fontSize:'13px', marginLeft:'5px'} }>
                             {memberIdMessage}
                         </div>
@@ -257,7 +279,7 @@ const EnterInfo = ():any => {
                     <span className="input-title">이름</span>
                     <div className="input-text">
                         <Styled.EnterInfoInput type="text" onChange={(data) => memberNameRegex(data.target.value)} placeholder="이름"
-                                               style={ isMemberNameEffect ? {} : {border: '2px solid red'} } />
+                                               style={ isMemberNameEffect ? {} : {border: '3px solid red'} } />
                         <div style={  isMemberNameEffect ? {display:'none'} : {display:'block', color:'red', fontSize:'13px', marginLeft:'5px'} }>
                             {memberNameMessage}
                         </div>
@@ -266,8 +288,12 @@ const EnterInfo = ():any => {
                 <div className="input-box">
                     <span className="input-title">비밀번호</span>
                     <div className="input-text">
-                        <Styled.EnterInfoInput type="password" onChange={(e) => memberPwRegex(e.target.value)} placeholder="비밀번호"
-                                               style={ isMemberPwEffect ? {} : {border: '2px solid red'} } />
+                        <div className="input-password">
+                            <Styled.EnterInfoInput type="password" onChange={(e) => memberPwRegex(e.target.value)} placeholder="비밀번호"
+                                                   style={ isMemberPwEffect ? {} : {border: '3px solid red'} } ref={passwordRef}/>
+                            <FontAwesomeIcon icon={passwordSeeIcon} className="icon-see"
+                                             onClick={() => passwordSeeHandler("")}/>
+                        </div>
                         <div style={  isMemberPwEffect ? {display:'none'} : {display:'block', color:'red', fontSize:'13px', marginLeft:'5px'} }>
                             {memberPwMessage}
                         </div>
@@ -276,8 +302,12 @@ const EnterInfo = ():any => {
                 <div className="input-box">
                     <span className="input-title">비밀번호 확인</span>
                     <div className="input-text">
-                        <Styled.EnterInfoInput type="password" onChange={(e) => memberPwChkRegex(e.target.value)} placeholder="비밀번호 확인"
-                                               style={ isMemberPwChkEffect ? {} : {border: '2px solid red'} } />
+                        <div className="input-password">
+                            <Styled.EnterInfoInput type="password" onChange={(e) => memberPwChkRegex(e.target.value)} placeholder="비밀번호 확인"
+                                                   style={ isMemberPwChkEffect ? {} : {border: '3px solid red'} } ref={passwordChkRef}/>
+                            <FontAwesomeIcon icon={passwordSeeIcon} className="icon-see"
+                                             onClick={() => passwordSeeHandler("chk")}/>
+                        </div>
                         <div style={  isMemberPwChkEffect ? {display:'none'} : {display:'block', color:'red', fontSize:'13px', marginLeft:'5px'} }>
                             {memberPwChkMessage}
                         </div>
@@ -308,7 +338,7 @@ const EnterInfo = ():any => {
                                                onBlur={() => {memberBirth.length >= 8 ?
                                                    setMemberBirth(memberBirth.substring(0, 4) + "." + memberBirth.substring(4, 6) + "." + memberBirth.substring(6))
                                                    : setMemberBirth(memberBirth)}} value={memberBirth}
-                                               style={ isMemberBirthEffect ? {} : {border: '2px solid red'} } />
+                                               style={ isMemberBirthEffect ? {} : {border: '3px solid red'} } />
                         <div style={  isMemberBirthEffect ? {display:'none'} : {display:'block', color:'red', fontSize:'13px', marginLeft:'5px'} }>
                             {memberBirthMessage}
                         </div>
@@ -318,7 +348,7 @@ const EnterInfo = ():any => {
                     <span className="input-title">전화번호</span>
                     <div className="input-text">
                         <Styled.EnterInfoInput type="text" onChange={(e) => memberPhoneRegex(e.target.value)} placeholder="- 없이 입력해주세요."
-                                               style={ isMemberPhoneEffect ? {} : {border: '2px solid red'} } />
+                                               style={ isMemberPhoneEffect ? {} : {border: '3px solid red'} } />
                         <div style={  isMemberPhoneEffect ? {display:'none'} : {display:'block', color:'red', fontSize:'13px', marginLeft:'5px'} }>
                             {memberPhoneMessage}
                         </div>
