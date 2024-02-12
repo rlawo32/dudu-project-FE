afb26d24ee0267418e0176d239f9c8a5873cb08a
rlawo <rlawo32@naver.com>
2024-02-12 10:56:04 +0900

    [DuDu-project][FE] <리뷰> 리뷰리스트_작업_20240212 (2)

--- src/member/memberInfoCompnent/LectureReview.style.tsx
+++ src/member/memberInfoCompnent/LectureReview.style.tsx
@@ -31,61 +31,9 @@ export const LectureReviewView = styled.div`
 
   .lr-main {
     width: 1160px;
-    margin: 30px auto 120px;
-
-    .lr-main-notice {
-      position: relative;
-      padding: 32px;
-      margin: 40px 0 30px 0;
-      border-radius: 12px;
-      background-color: #f7f2ef;
-      color: black;
-
-      ul {
-        padding-left: 20px;
-
-        li {
-          line-height: 32px;
-          opacity: 0.7;
-        }
-      }
-    }
+    margin: 70px auto 120px;
 
     .lr-main-list {
-
-      .lr-list-search {
-        position: relative;
-        width: 600px;
-        @media screen and (max-width: 1024px) {
-          box-sizing: border-box;
-          width: 100%;
-          padding: 0 20px;
-        }
-        margin: 40px auto 0;
-
-        input {
-          box-sizing: border-box;
-          height: 52px;
-          width: 100%;
-          padding: 0 80px 0 25px;
-          background-color: ${({theme}) => theme.boxBgColor};
-          color: ${({theme}) => theme.textColor};
-          border: 1px solid ${({theme}) => theme.textColor};
-          border-radius: 30px;
-          font-size: 20px;
-        }
-
-        .icon-custom {
-          position: absolute;
-          top: 10px;
-          right: 30px;
-          font-size: 30px;
-          cursor: pointer;
-          @media screen and (max-width: 1024px) {
-            right: 50px;
-          }
-        }
-      }
 
       .lr-list-view {
 
@@ -93,10 +41,303 @@ export const LectureReviewView = styled.div`
           display: flex;
           justify-content: space-between;
           align-items: center;
+          height: 68px;
+
+          .list-top-left {
+            font-weight: bold;
+
+            span {
+              font-weight: normal;
+              opacity: 0.7;
+            }
+          }
+
+          .list-top-right {
+            position: relative;
+
+            button {
+              border: none;
+              background: none;
+              color: ${({theme}) => theme.textColor};
+              font-size: 16px;
+              @media screen and (max-width: 1024px) {
+                font-size: 13px;
+              }
+              font-weight: bold;
+              cursor: pointer;
+            }
+
+            .sort-box {
+              position: absolute;
+              top: 105%;
+              right: 0;
+              height: 0;
+              width: 70px;
+              padding: 0;
+              margin: 5px auto 0;
+              border: none;
+              border-radius: 5px;
+              background: ${({theme}) => theme.boxBgColor};
+              text-align: center;
+              z-index: 2;
+              transition: all 0.3s ease-in;
+            }
+
+            ul.sort-list {
+              height: 0;
+              width: 60px;
+              padding: 0;
+              margin: 5px auto 0;
+              border: none;
+              overflow: auto;
+              background: ${({theme}) => theme.boxBgColor};
+              color: ${({theme}) => theme.textColor};
+              text-align: center;
+              cursor: pointer;
+              z-index: 3;
+              user-select: none;
+              list-style:none;
+              transition: all 0.3s ease-in;
+
+              &::-webkit-scrollbar {
+                width: 5px;
+              }
+
+              &::-webkit-scrollbar-thumb {
+                background: gray; /* 스크롤바의 색상 */
+                border-radius: 15px;
+              }
+
+              &::-webkit-scrollbar-track {
+                background: rgba(200, 200, 200, .1);
+              }
+            }
+
+            ul.sort-list li {
+              padding: 5px;
+              font-size: 12px;
+              line-height: 1.4em;
+              opacity: 0.7;
+              transition: all 0.3s ease-in;
+            }
+
+            .select-arrow {
+              display: inline-block;
+              margin-left: 7px;
+              transition: all .4s linear;
+            }
+
+            .sort-box.show-list {
+              border: 1px solid gray;
+              padding: 5px;
+              height: 100px;
+              width: 70px;
+            }
+
+            .sort-list.show-list {
+              border: none;
+              padding: 5px;
+              height: 85px;
+              width: 60px;
+            }
+
+            ul.sort-list li.sort-active {
+              opacity: 1;
+              font-weight: bold;
+            }
+
+            .select-arrow.show-list {
+              transform: rotate(180deg);
+            }
+          }
         }
 
         .lr-list {
+          display: flex;
+          align-items: flex-start;
+          flex-wrap: wrap;
+          margin-top: 20px;
+          @media screen and (max-width: 1024px) {
+            flex-direction: column;
+            padding: 0 10px;
+          }
+          
+          .lr-list-item {
+            width: calc((100% - 225px) / 2);
+            padding: 30px;
+            margin: 0 25px 30px;
+            border: 1px solid #ddcdc5;
+            border-radius: 12px;
+            background-color: ${({theme}) => theme.cardBgColor};
+            @media screen and (max-width: 1024px) {
+              box-sizing: border-box;
+              width: 100%;
+              margin: 20px 0;
+            }
+            
+            .item-head {
+              box-sizing: border-box;
+              position: relative;
+              top: -15px;
+              left: -15px;
+              display: flex;
+              justify-content: space-between;
+              align-items: center;
+              min-height: 56px;
+              width: calc(100% + 30px);
+              padding: 10px 15px;
+              margin-bottom: 5px;
+              border-radius: 12px;
+              background-color: ${({theme}) => theme.boxBgColor};
+              
+              .head-value {
+                display: flex;
+                font-size: 14px;
+                font-weight: 500;
+                line-height: 22px;
+                letter-spacing: -.35px;
+                
+                div {
+                  width: 60px;
+                }
+              }
+              
+              .payment-cancel-btn {
+                display: inline-block;
+                min-height: 10px;
+                min-width: 50px;
+                padding: 8px 5px 9px;
+                border: 2px solid ${({theme}) => theme.rgbaMedium};
+                border-radius: 8px;
+                background-color: ${({theme}) => theme.bgColor};
+                color: ${({theme}) => theme.textColor};
+                font-size: 12px;
+                font-weight: bold;
+                text-align: center;
+                transition: all .4s ease;
+                cursor: pointer;
+              }
+            }
+            
+            .item-body {
+              padding-bottom: 20px;
+              border-bottom: 1px solid ${({theme}) => theme.rgbaMedium};
+              
+              .body-value {
+                font-size: 14px;
+                line-height: 22px;
+                word-break: break-all;
+              }
+              
+              .body-top {
+                width: 100%;
+                
+                .item-institution {
+                  width: fit-content;
+                  padding: 0 6px;
+                  margin: 0 2px 2px 0;
+                  border: 1px solid ${({theme}) => theme.rgbaMedium};
+                  border-radius: 9px;
+                  font-size: 11px;
+                  line-height: 16px;
+                  white-space: nowrap;
+                }
+                
+                .item-title {
+                  min-height: 65px;
+                  margin-top: 4px;
+                  font-size: 20px;
+                  font-weight: bold;
+                  line-height: 32px;
+                  letter-spacing: -.6px;
+                  overflow: hidden;
+                  text-overflow: ellipsis;
+                  white-space: normal;
+                  word-break: keep-all;
+                  cursor: pointer;
+                }
+              }
+              
+              .body-bot {
+                margin-top: 16px;
+                
+                .item-period {
+                  
+                  span {
+                    margin-left: 3px;
+                  }
+                }
+                
+                .item-fee {
+                  font-weight: bold;
+                  
+                  span {
+                    margin-right: 3px;
+                    font-weight: normal;
+                  }
+                }
+              }
+            }
+            
+            .item-foot {
 
+              button {
+                height: 44px;
+                width: 100%;
+                padding: 2px 8px 4px;
+                margin-top: 20px;
+                border: 1px solid ${({theme}) => theme.rgbaMedium};
+                border-radius: 8px;
+                background-color: #e0f55c;
+                color: black;
+                text-align: center;
+                font-size: 14px;
+                font-weight: bold;
+                line-height: 38px;
+                letter-spacing: -.75px;
+                cursor: pointer;
+              }
+            }
+          }
+        }
+
+        .lh-list-empty {
+          margin: 60px auto;
+          text-align: center;
+          color: grey;
+          font-size: 25px;
+
+          .icon-custom {
+            margin: 15px 0 15px 0;
+            font-size: 70px;
+          }
+
+          .empty-text {
+
+            .search-text {
+              color: ${({theme}) => theme.textColor};
+              font-weight: bold;
+            }
+
+            .icon-custom {
+              margin: 8px 3px 8px 3px;
+              color: ${({theme}) => theme.textColor};
+              font-size: 20px;
+            }
+          }
+
+        }
+
+        .lh-more-btn {
+          width: 25%;
+          margin: 40px auto 0;
+          padding: 10px 15px 10px 15px;
+          border: 1px solid ${({theme}) => theme.textColor};
+          border-radius: 10px;
+          text-align: center;
+          font-size: 20px;
+          font-weight: bold;
+          cursor: pointer;
         }
       }
     }
--- src/member/memberInfoCompnent/LectureReview.tsx
+++ src/member/memberInfoCompnent/LectureReview.tsx
@@ -1,16 +1,122 @@
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
+import axios from "axios";
 
 import HeaderNavigation from "../../navigation/HeaderNavigation";
 import FooterNavigation from "../../navigation/FooterNavigation";
 
 import * as Styled from "./LectureReview.style";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faSearch as searchIcon} from "@fortawesome/free-solid-svg-icons";
+import {
+    faChevronDown as arrow,
+    faExclamation as emptyIcon, faQuoteLeft as quoteLeft, faQuoteRight as quoteRight,
+    faSearch as searchIcon
+} from "@fortawesome/free-solid-svg-icons";
+import {useNavigate} from "react-router-dom";
 
 const LectureReview = () => {
+    const navigate = useNavigate();
+    const sortBox:any = useRef<any>();
+    const sortList:any = useRef<any>();
+    const sortBtn:any = useRef<any>([]);
+    const selectArrow:any = useRef<any>();
 
-    const [isSearchText, setIsSearchText] = useState<boolean>(false);
-    const [searchText, setSearchText] = useState<string>("");
+    const [pageNo, setPageNo] = useState<number>(1);
+    const [totalPage, setTotalPage] = useState<number>(0);
+
+    const sortItem:string[] = ['2024', '2023'];
+    const [sortType, setSortType] = useState<string>("");
+    const [sortSelect, setSortSelect] = useState<number>(0);
+    const [isSortBoxShow, setIsSortBoxShow] = useState<boolean>(false);
+
+    const [lectureReviewList, setLectureReviewList] = useState<{
+        lectureApplicationNo:number;
+        lectureApplicationOrderId:string;
+        lectureApplicationAmount:number;
+        lectureApplicationCancelYn:string;
+        lectureApplicationCancelDesc:string;
+        lectureApplicationCreatedDate:string;
+        lectureApplicationMemberName:string;
+        lectureNo:number;
+        lectureInstitutionName:string;
+        lectureTitle:string;
+        lectureTeacher:string;
+        lecturePeriod:string;
+        lectureTime:string;
+        lectureCount:number;
+        lectureFee:number;
+        lectureStateNo:number;
+    }[]>([]);
+
+    const sortItemList = ():any[] => {
+        let result:any[] = [];
+        for(let i:number=0; i<=sortItem.length; i++) {
+            if(i === 0) {
+                result.push(<li key={i}
+                                ref={btn => (sortBtn.current[i] = btn)}
+                                onClick={() => onClickSortSelectBox(i, "")}>
+                    전체</li>)
+            } else {
+                result.push(<li key={i}
+                                ref={btn => (sortBtn.current[i] = btn)}
+                                onClick={() => onClickSortSelectBox(i, sortItem[i-1])}>
+                    {sortItem[i-1]}</li>)
+            }
+        }
+        return result;
+    }
+
+    const onClickSortSelectBox = (idx:number, sortSelect:string):void => {
+        setIsSortBoxShow(false);
+        setSortSelect(idx);
+        setSortType(sortSelect);
+    }
+
+    useEffect(() => {
+        const getListData:object = {
+            pageNo: pageNo,
+            searchCategory: 'R',
+            sortType: sortType,
+            searchText: ''
+        }
+        const lectureReviewListData = async () => {
+            await axios({
+                method: "POST",
+                url: '/lecture/lectureApplicationList',
+                data: JSON.stringify(getListData),
+                headers: {'Content-type': 'application/json'}
+            }).then((res):void => {
+                setLectureReviewList(res.data.data.applicationList);
+                setTotalPage(res.data.data.totalPage);
+            }).catch((err):void => {
+                console.log(err.message);
+            });
+        }
+        setTimeout(() => {lectureReviewListData().then();}, 200);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [pageNo, sortType])
+
+    useEffect(() => {
+        if(isSortBoxShow) {
+            sortBox.current.className += " show-list";
+            sortList.current.className += " show-list";
+            selectArrow.current.className += " show-list";
+        } else {
+            sortBox.current.className = sortBox.current.className.replace(' show-list', '');
+            sortList.current.className = sortList.current.className.replace(' show-list', '');
+            selectArrow.current.className = selectArrow.current.className.replace(' show-list', '');
+        }
+    }, [isSortBoxShow])
+
+    useEffect(() => {
+        sortBtn.current[sortSelect].className = sortBtn.current[sortSelect].className.replace('sort-active', '');
+        sortBtn.current[sortSelect].className += 'sort-active';
+
+        for(let i:number=0; i<sortBtn.current.length; i++) {
+            if(i !== sortSelect) {
+                sortBtn.current[i].className = sortBtn.current[i].className.replace('sort-active', '');
+            }
+        }
+    }, [sortSelect])
 
     return (
         <Styled.LectureReviewView>
@@ -25,32 +131,126 @@ const LectureReview = () => {
             </div>
 
             <div className="lr-main">
-                <div className="lr-main-notice">
-                    <ul>
-                        <li>데스크에서 접수한 강좌의 경우 방문 시에만 취소 가능합니다. (결제한 카드 및 영수증 지참 필수)</li>
-                        <li>재료 준비가 필요한 일부 강좌(요리, 공예, 플라워 등)는 강좌 시작일의 3일 전까지 취소 가능합니다.</li>
-                        <li>환불 및 수강 취소시 강의시작일로부터 3일전은 환불액에서 1/3 환급, 2일전은 1/2 환급, 전날부터는 환불이 불가합니다.</li>
-                    </ul>
-                </div>
                 <div className="lr-main-list">
-                    <div className="lr-list-search">
-                        <input type="text" onChange={(e) => setSearchText(e.target.value)}
-                               placeholder="주문번호/강좌명으로 검색하세요" />
-                        <FontAwesomeIcon icon={searchIcon} className="icon-custom"
-                                         onClick={() => setIsSearchText(!isSearchText)}/>
-                    </div>
                     <div className="lr-list-view">
                         <div className="lr-list-top">
                             <div className="list-top-left">
-                                전체 1개
+                                <span>전체</span> {totalPage}개
                             </div>
-                            <div className="list-top-right">
-                                전체연도
-                            </div>
-                        </div>
-                        <div className="lr-list">
 
+                            <div className="list-top-right">
+                                <button onClick={() => setIsSortBoxShow(!isSortBoxShow)}>
+                                    {
+                                        sortSelect === 0 ?
+                                            "전체"
+                                            :
+                                            sortItem[sortSelect-1] + "년"
+                                    }
+                                    <div className="select-arrow" ref={selectArrow}>
+                                        <FontAwesomeIcon icon={arrow} />
+                                    </div>
+                                </button>
+                                <div className="sort-box" ref={sortBox}>
+                                    <ul className="sort-list" ref={sortList}>
+                                        {sortItemList()}
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
+                        {
+                            lectureReviewList.length > 0 ?
+                                <div className="lr-list">
+                                    {lectureReviewList.map((item, idx) => (
+                                        <div key={idx} className="lr-list-item">
+                                            <div className="item-head">
+                                                <div className="head-left">
+                                                    <div className="head-value item-orderId">
+                                                        <div>
+                                                            주문번호
+                                                        </div>
+                                                        {item.lectureApplicationOrderId}
+                                                    </div>
+                                                    <div className="head-value item-date">
+                                                        <div>
+                                                            {
+                                                                item.lectureApplicationCancelYn === 'N' ?
+                                                                    "결제일" : "취소일"
+                                                            }
+                                                        </div>
+                                                        {item.lectureApplicationCreatedDate}
+                                                    </div>
+                                                </div>
+
+                                                <div className="head-rigth">
+                                                    <div className="payment-cancel-btn">
+                                                        수강완료
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="item-body">
+                                                <div className="body-value body-top">
+                                                    <div className="item-institution">
+                                                        {item.lectureInstitutionName}
+                                                    </div>
+                                                    <div className="item-title"
+                                                         onClick={() => navigate("/lectureDetail/" + item.lectureNo,
+                                                             { state: {lectureNo: item.lectureNo}})}>
+                                                        {item.lectureTitle}
+                                                    </div>
+                                                </div>
+                                                <div className="body-value body-bot">
+                                                    <div className="item-teacher">
+                                                        {item.lectureTeacher}
+                                                    </div>
+                                                    <div className="item-period">
+                                                        {item.lecturePeriod}
+                                                        <span>
+                                                            {
+                                                                item.lectureTime.substring(13, 14) === '1' ? '(월)' :
+                                                                    item.lectureTime.substring(13, 14) === '2' ? '(화)' :
+                                                                        item.lectureTime.substring(13, 14) === '3' ? '(수)' :
+                                                                            item.lectureTime.substring(13, 14) === '4' ? '(목)' :
+                                                                                item.lectureTime.substring(13, 14) === '5' ? '(금)' :
+                                                                                    item.lectureTime.substring(13, 14) === '6' ? '(토)' : '(일)'
+                                                            }
+                                                        </span>
+                                                        <span>
+                                                            {item.lectureTime.substring(0, 11)}
+                                                        </span>
+                                                        <span>/ {item.lectureCount}회</span>
+                                                    </div>
+                                                    <div className="item-fee">
+                                                        <span>강좌료 :</span>
+                                                        {item.lectureFee.toLocaleString()}원
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="item-foot">
+                                                <button onClick={() => navigate("/reviewWrite")}>
+                                                    후기 작성하기
+                                                </button>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                                :
+                                <div className="lh-list-empty">
+                                    <div>
+                                        <FontAwesomeIcon icon={emptyIcon} className="icon-custom" />
+                                        <div className="default-text">
+                                            수강내역이 없습니다.
+                                        </div>
+                                    </div>
+                                </div>
+                        }
+                        {
+                            totalPage > lectureReviewList.length ?
+                                <div className="lh-more-btn" onClick={() => setPageNo(pageNo + 1)}>
+                                    더보기 <FontAwesomeIcon icon={arrow} />
+                                </div>
+                                :
+                                <div />
+                        }
                     </div>
                 </div>
             </div>
