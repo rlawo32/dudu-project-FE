d41c841b1ee6b52fd11d805beaed073e0cdbbc45
rlawo <rlawo32@naver.com>
2024-02-12 14:24:17 +0900

    [DuDu-project][FE] <리뷰> 리뷰작성_작업_20240212 (2)

--- src/community/ReviewWrite.style.tsx
+++ src/community/ReviewWrite.style.tsx
@@ -1,6 +1,83 @@
 import styled from "styled-components";
 
 export const ReviewWriteView = styled.div`
+  position: absolute;
+  top: 10%;
+  left: 50%;
+  transform: translate(-50%, -10%);
+  height: 670px;
+  width: 600px;
+  padding: 30px;
+  border: 1px solid ${({theme}) => theme.textColor};
+  border-radius: 15px;
+  background-color: ${({theme}) => theme.bgColor};
+  z-index: 5;
+  
+  .item-title {
+    margin-bottom: 5px;
+    font-weight: bold;
+  }
+  
+  .rw-view {
+    
+    .rw-view-top {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 100%;
+      margin: 15px auto 0;
+      
+      .rw-title {
+        width: 60%;
 
+        input {
+          box-sizing: border-box;
+          width: 100%;
+          border: ${({theme}) => theme.borderColor};
+          border-radius: 10px;
+          padding: 10px;
+          margin-top: 5px;
+          font-size: 16px;
+          font-weight: bold;
+          background-color: ${({theme}) => theme.boxBgColor};
+          color: ${({theme}) => theme.textColor};
+        }
+      }
+      
+      .rw-score {
+        width: 35%;
+        margin-left: 30px;
+        
+        .score-view {
+
+          .score-item {
+            display: inline-block;
+            font-size: 35px;
+          }
+        }
+      }
+    }
+    
+    .rw-content {
+      margin-top: 15px;
+      
+    }
+    
+    .rw-button {
+      margin: 15px auto 0;
+      text-align: center;
+      
+      button {
+        border: ${({theme}) => theme.borderColor};
+        border-radius: 10px;
+        padding: 10px;
+        background-color: ${({theme}) => theme.reverseBgColor};
+        color: ${({theme}) => theme.reverseTextColor};
+        font-size: 20px;
+        font-weight: bold;
+        cursor: pointer;
+      }
+    }
+  }
 
 `;
--- src/community/ReviewWrite.tsx
+++ src/community/ReviewWrite.tsx
@@ -1,12 +1,112 @@
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
 
+import LectureQuillEditor from "../lecture/lectureWriteComponent/LectureQuillEditor";
 
+import * as Styled from "./ReviewWrite.style";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faStar as fullStar} from "@fortawesome/free-solid-svg-icons";
+import {faStar as emptyStar} from "@fortawesome/free-regular-svg-icons";
 
-const ReviewWrite = () => {
+interface Props {
+    setIsModal: React.Dispatch<React.SetStateAction<boolean>>;
+    lectureNo:number;
+}
+
+const ReviewWrite = (props:Props) => {
+    const navigate = useNavigate();
+
+    const [reviewTitle, setReviewTitle] = useState<string>("");
+    const [reviewContent, setReviewContent] = useState<string>("");
+    const [reviewScore, setReviewScore] = useState<number>(0);
+    const [reviewImageArr, setReviewImageArr] = useState<{
+        imgType:string;
+        imgName:string;
+        imgUrl:string;
+        imgSize:number;
+    }[]>([]);
+
+    const customRatingStarActionHandler = ():any[] => {
+        let result:any[] = [];
+        for (let i:number=0; i<5; i++) {
+            result.push(
+                <div key={i+1} onClick={() => setReviewScore(i+1)} className="score-item">
+                        {
+                            i+1 <= reviewScore ?
+                                <FontAwesomeIcon icon={fullStar} />
+                                :
+                                <FontAwesomeIcon icon={emptyStar} />
+                        }
+                    </div>);
+        }
+        return result;
+    }
+
+    const reviewWriteHandler = ():boolean => {
+        const reviewData:object = {
+            lectureNo: props.lectureNo,
+            reviewTitle: reviewTitle,
+            reviewContent: reviewContent,
+            reviewScore: reviewScore,
+            reviewImage: reviewImageArr
+        }
+        if(reviewTitle.length < 1) {
+            alert('제목을 작성해주세요.');
+            return false;
+        } else if(reviewContent.length < 1) {
+            alert('내용을 작성해주세요.');
+            return false;
+        } else if(reviewScore < 1) {
+            alert('별점을 최소 1점이상을 주세요.');
+            return false;
+        } else {
+            if(window.confirm("작성하시겠습니까?") == true) {
+                // axios({
+                //     method: "POST",
+                //     url: "/review/reviewWrite",
+                //     data: JSON.stringify(reviewData),
+                //     headers: {'Content-type': 'application/json'}
+                // }).then((res):void => {
+                //     alert("작성이 완료되었습니다.");
+                //     navigate("/");
+                // }).catch((err):void => {
+                //     console.log(err.message);
+                // })
+                console.log(reviewData)
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 
     return (
-        <div>
-            
-        </div>
+        <Styled.ReviewWriteView>
+            <div className="rw-view">
+                <div className="rw-view-top">
+                    <div className="rw-title">
+                        <div className="item-title">제목 작성</div>
+                        <input type="text" value={reviewTitle}
+                               onChange={(e) => setReviewTitle(e.target.value)}/>
+                    </div>
+                    <div className="rw-score">
+                        <div className="item-title">별점</div>
+                        <div className="score-view">
+                            {customRatingStarActionHandler()}
+                        </div>
+                    </div>
+                </div>
+                <div className="rw-content">
+                    <div className="item-title">내용 작성</div>
+                    <LectureQuillEditor useType={"R"} content={reviewContent} setContent={setReviewContent}
+                                        Image={reviewImageArr} setImage={setReviewImageArr}/>
+                </div>
+                <div className="rw-button">
+                    <button onClick={() => reviewWriteHandler()}>작성하기</button>
+                </div>
+            </div>
+        </Styled.ReviewWriteView>
     )
 }
 
