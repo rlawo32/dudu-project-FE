da1333e086a973f3bfefd1b8d024d757fb8035ab
rlawo <rlawo32@naver.com>
2024-02-13 15:42:02 +0900

    [DuDu-project][FE] <리뷰> 리뷰디테일_작업_20240213 (1)

--- src/community/ReviewDetail.style.tsx
+++ src/community/ReviewDetail.style.tsx
@@ -1,6 +1,162 @@
 import styled from "styled-components";
 
 export const ReviewDetailView = styled.div`
+  position: relative;
+  padding-top: 80px;
+  margin: 70px auto 0;
 
+  .ld-main-view {
+    width: 1160px;
+    @media screen and (max-width: 1280px) {
+      box-sizing: border-box;
+      width: 100%;
+      padding: 20px;
+    }
+    padding: 60px 0 200px;
+    margin: 0 auto;
 
+    .ld-head {
+      padding-bottom: 40px;
+      border-bottom: 1px solid ${({theme}) => theme.rgbaMedium};
+
+      .head-top {
+        display: flex;
+        align-items: center;
+        font-size: 13px;
+        line-height: 19px;
+        opacity: 0.5;
+
+        .head-item {
+          position: relative;
+          float: left;
+          padding-right: 8px;
+          margin-right: 8px;
+          word-break: break-all;
+
+          &:after {
+            display: block;
+            content: '';
+            position: absolute;
+            top: 5px;
+            right: 0;
+            width: 1px;
+            height: 10px;
+            background-color: #dfd9d5;
+          }
+        }
+
+        .bd-institution {
+
+        }
+
+        .bd-date {
+
+        }
+      }
+
+      .head-bottom {
+
+        .ld-title {
+          margin-top: 10px;
+          font-size: 32px;
+          font-weight: bold;
+          line-height: 40px;
+        }
+        
+        .ld-score {
+          display: flex;
+          justify-content: flex-end;
+          visibility: visible;
+          padding-right: 8px;
+          margin-right: 8px;
+        }
+      }
+    }
+
+    .ld-body {
+
+      .ld-content {
+        padding: 80px 68px 100px;
+        border-bottom: 1px solid ${({theme}) => theme.rgbaMedium};
+        text-align: center;
+      }
+      
+      .ld-lt-info {
+        
+        .info-head {
+          
+        }
+        
+        .info-body {
+          display: flex;
+          
+          .ld-lt-left {
+            
+          }
+          
+          .ld-lt-right {
+            
+            .ld-lt-top {
+              
+              .ld-lt-state {
+                
+              }
+              
+              .ld-lt-mainCategory {
+                
+              }
+              
+              .ld-lt-subCategory {
+                
+              }
+            }
+            
+            .ld-lt-mid {
+              
+              .ld-lt-title {
+                
+              }
+            }
+            
+            .ld-lt-bot {
+              
+              .ld-lt-institution {
+                
+              }
+              
+              .ld-lt-teacher {
+                
+              }
+              
+              .ld-lt-period {
+                
+              }
+            }
+          }
+        }
+      }
+    }
+
+    .ld-foot {
+      display: flex;
+      justify-content: center;
+      margin: 40px 0;
+
+      button {
+        display: inline-block;
+        padding: 16px 10px 17px;
+        min-height: 60px;
+        min-width: 160px;
+        border-radius: 8px;
+        background-color: ${({theme}) => theme.reverseBgColor};
+        color: ${({theme}) => theme.reverseTextColor};
+        text-align: center;
+        font-size: 20px;
+        font-weight: bold;
+        line-height: 24px;
+        letter-spacing: -.75px;
+        cursor: pointer;
+      }
+    }
+  }
 `;
--- src/community/ReviewDetail.tsx
+++ src/community/ReviewDetail.tsx
@@ -1,10 +1,155 @@
+import React, {useEffect, useState} from "react";
+import {useLocation, useNavigate} from "react-router-dom";
+import dompurify from "dompurify";
+import axios from "axios";
+
+import HeaderNavigation from "../navigation/HeaderNavigation";
+import FooterNavigation from "../navigation/FooterNavigation";
+import TopButtonNavigation from "../navigation/TopButtonNavigation";
+
+import * as Styled from "./ReviewDetail.style";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faStar as fullStar} from "@fortawesome/free-solid-svg-icons";
+import {faStar as emptyStar} from "@fortawesome/free-regular-svg-icons";
 
 const ReviewDetail = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const reviewNo:number = location.state.reviewNo;
+
+    const sanitizer = dompurify.sanitize;
+    // 스크립트를 활용한 토큰 탈취 처럼 취약점을 노려서 javascript와 HTML로 악의적 코드를 웹 브라우저에 심어,
+    // 사용자 접속시 그 악성코드가 실행되는 것을 크로스 사이드 스크립트, 보안을 위해 추가
+
+    const [reviewDetail, setReviewDetail] = useState<{
+        reviewNo:number; reviewTitle:string; reviewContent:string; reviewAuthor:string;
+        reviewScore:number; reviewCreatedDate:string;
+        lectureNo:number; lectureStateNo:number; institutionName:string; mainCategoryName:string;
+        subCategoryName:string; lectureTitle:string; lectureDivision:string; lectureTeacher:string;
+        lecturePeriod:string; lectureThumbnail:string;
+    }>();
+
+    const customReviewRatingArr = (score:undefined|number) => {
+        let result:any[] = [];
+        if(score !== undefined) {
+            for (let i:number=0; i<5; i++) {
+                result.push(
+                    <span key={i+1} className="rating">
+                        {
+                            i+1 <= score ?
+                                <FontAwesomeIcon icon={fullStar} />
+                                :
+                                <FontAwesomeIcon icon={emptyStar} />
+                        }
+                    </span>);
+            }
+        }
+        return result;
+    }
+
+    useEffect(() => {
+        const reviewDetailData = async () => {
+            await axios({
+                method: "GET",
+                url: '/review/reviewDetail',
+                params: {reviewNo: reviewNo}
+            }).then((res):void => {
+                setReviewDetail(res.data.data);
+            }).catch((err):void => {
+                console.log(err.message);
+            });
+        }
+        setTimeout(() => {reviewDetailData().then();}, 100);
+    }, [])
 
     return (
-        <div>
-            
-        </div>
+        <Styled.ReviewDetailView>
+            <HeaderNavigation />
+
+            <div className="ld-main-view">
+                <div className="ld-head">
+                    <div className="head-top">
+                        <div className="head-item ld-author">
+                            {reviewDetail?.reviewAuthor}
+                        </div>
+                        <div className="head-item ld-date">
+                            {reviewDetail?.reviewCreatedDate.substring(0, 10)}
+                        </div>
+                    </div>
+                    <div className="head-bottom">
+                        <div className="ld-title">
+                            {reviewDetail?.reviewTitle}
+                        </div>
+                        <div className="ld-score">
+                            {customReviewRatingArr(reviewDetail?.reviewScore)}
+                        </div>
+                    </div>
+                </div>
+                <div className="ld-body">
+                    <div className="ld-content"
+                         dangerouslySetInnerHTML={{ __html : sanitizer(`${reviewDetail?.reviewContent}`) }} />
+                    <div className="ld-lt-Info">
+                        <div className="info-head">수강 정보</div>
+                        <div className="info-body"
+                             onClick={() => navigate("/lectureDetail/" + reviewDetail?.lectureNo,
+                            { state: {lectureNo: reviewDetail?.lectureNo}})}>
+                            <div className="ld-lt-left">
+                                <img src={reviewDetail?.lectureThumbnail} alt={"강좌 이미지"}/>
+                            </div>
+                            <div className="ld-lt-right">
+                                <div className="ld-lt-top">
+                                    <div className="ld-lt-state" style={
+                                        reviewDetail?.lectureStateNo === 1 ? {backgroundColor: "slategray", color: 'white'} :
+                                            reviewDetail?.lectureStateNo === 2 ? {backgroundColor: "greenyellow", color: 'black'} :
+                                                reviewDetail?.lectureStateNo === 3 ? {backgroundColor: "slategray", color: 'black'} :
+                                                    reviewDetail?.lectureStateNo === 4 ? {backgroundColor: "black", color: 'white'} :
+                                                        reviewDetail?.lectureStateNo === 5 || 6 ? {backgroundColor: "red", color: 'black'} : {}}>
+                                        {
+                                            reviewDetail?.lectureStateNo === 1 ? '접수예정' :
+                                                reviewDetail?.lectureStateNo === 2 ? '접수중' :
+                                                    reviewDetail?.lectureStateNo === 3 ? '대기접수' :
+                                                        reviewDetail?.lectureStateNo === 4 ? '접수마감' :
+                                                            reviewDetail?.lectureStateNo === 5 ? '접수불가' : '강의종료'
+                                        }
+                                    </div>
+                                    <div className="ld-lt-mainCategory">
+                                        {reviewDetail?.mainCategoryName}
+                                    </div>
+                                    <div className="ld-lt-subCategory">
+                                        {reviewDetail?.subCategoryName}
+                                    </div>
+                                </div>
+                                <div className="ld-lt-mid">
+                                    <div className="ld-lt-title">
+                                        {reviewDetail?.lectureTitle}
+                                    </div>
+                                </div>
+                                <div className="ld-lt-bot">
+                                    <div className="ld-lt-institution">
+                                        {reviewDetail?.institutionName}
+                                    </div>
+                                    <div className="ld-lt-teacher">
+                                        {reviewDetail?.lectureTeacher}
+                                    </div>
+                                    <div className="ld-lt-period">
+                                        {reviewDetail?.lecturePeriod}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="ld-foot">
+                    <button onClick={() => navigate("/reviewList")}>
+                        목록으로
+                    </button>
+                </div>
+            </div>
+
+            <TopButtonNavigation type={""} />
+
+            <FooterNavigation />
+        </Styled.ReviewDetailView>
     )
 }
 
