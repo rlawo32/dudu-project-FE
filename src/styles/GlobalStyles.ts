8890cb81b294f2c636848c8413600378fa4590ad
rlawo <rlawo32@naver.com>
2024-02-16 14:42:01 +0900

    [DuDu-project][FE] <JWT> 추가_작업_20240216 (6)

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,11 +1,20 @@
 import { createGlobalStyle } from "styled-components";
 
-export const GlobalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle<{$isModal:boolean}>`
     body {
-        background: ${({ theme }) => theme.bgColor};
-        color: ${({ theme }) => theme.textColor};
-        display: block;
-        height: 100%;
-        width: 100%;
+      background: ${({ theme }) => theme.bgColor};
+      color: ${({ theme }) => theme.textColor};
+      display: block;
+      height: 100%;
+      width: 100%;
+
+      overflow: ${({$isModal}) => $isModal ? "hidden" : "auto"};
+      
+      #root {
+        div:nth-child(2) > * {
+          opacity: ${({$isModal}) => $isModal ? 0.5 : 1};
+          pointer-events: ${({$isModal}) => $isModal ? "none" : "auto"};
+        }
+      }
     }
 `;
