a26ff98c39ede8a9d12ac3e93251938b7964486c
rlawo <rlawo32@naver.com>
2024-02-24 03:53:00 +0900

    [DuDu-project][FE] <로그아웃> SAFE_LOGOUT_작업_20240224 (2)

--- src/stores/useTokenExpiresStore.ts
+++ src/stores/useTokenExpiresStore.ts
@@ -1,12 +1,25 @@
 import { create } from "zustand";
 
 interface tokenExpiresStore {
+    isTokenExpiresTimeStart: boolean;
+    setIsTokenExpiresTimeStart: (time: boolean) => void;
+    isTokenExpiresTimeBox: boolean;
+    setIsTokenExpiresTimeBox: (box: boolean) => void;
     tokenExpiresTime: number;
     setTokenExpiresTime: (decrease: number) => void;
 }
 
 const useTokenExpiresStore = create<tokenExpiresStore>((set) => ({
-    // 초기
+    isTokenExpiresTimeStart: false,
+    setIsTokenExpiresTimeStart: (time: boolean) =>
+        set((state: {isTokenExpiresTimeStart: boolean}) => ({
+            isTokenExpiresTimeStart: (state.isTokenExpiresTimeStart = time),
+        })),
+    isTokenExpiresTimeBox: false,
+    setIsTokenExpiresTimeBox: (box: boolean) =>
+        set((state: {isTokenExpiresTimeBox: boolean}) => ({
+            isTokenExpiresTimeBox: (state.isTokenExpiresTimeBox = box),
+        })),
     tokenExpiresTime: 0,
     setTokenExpiresTime: (decrease: number) =>
         set((state: {tokenExpiresTime: number}) => ({
